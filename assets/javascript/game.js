bbb17af84c9cb48566805d6f4a6224fed5df6118
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-06-08 09:07:59 -0400

    js working except for list of guessed letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,26 +3,44 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var losses = 0;
 var numGuesses = 9;
-var guessChoices = [];
+var guessesSoFar = [];
 
 // Computer Letter Choice and User Guess
 document.onkeypress = function(event) {
     var userGuess = event.key;
     var computerChoice = letters[Math.floor(Math.random() * letters.length)];
 
-// Game Loop
-if(userGuess === computerChoice){
-    wins++;
-}else{
-    guesses--;
-}
+// Game Loop - Win
+if (letters.indexOf(userGuess) > -1) {
+    if (userGuess === computerChoice) {
+        wins++;
+        numGuesses = 9;
+        guessesSoFar = [];
+    }
+    else {
 
-if(guesses === 0){
-    losses++
-}
+// Game Loop - Already Guessed
+        if (guessesSoFar.includes(userGuess)) {
+        }
+
+// Game Loop - Lose Life
+        else {
+            numGuesses--;
+            guessesSoFar.push(userGuess);
+        }
+    }
+
+// Game Loop - Lose Game
+    if (numGuesses === 0) {
+        numGuesses = 9;
+        losses++;
+        guessesSoFar = [];
+    }
+}   
 
 // Rewrite Stats
-document.getElementById('wins').innerHTML = "Wins: " + wins;
-document.getElementById('losses').innerHTML = "losses: " + losses;
-document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
+document.getElementById("winsStat").innerHTML = "" + wins;
+document.getElementById("lossesStat").innerHTML = "" + losses;
+document.getElementById("guessesLeftStat").innerHTML = "" + numGuesses;
+document.getElementById("guessesSoFarStat").innerHTML = "" + userGuess;
 }
